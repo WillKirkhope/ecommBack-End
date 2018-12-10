944b5221df35851ea50ca74edfe0850604d05546
WillKirkhope <kirkhopetraining@yahoo.com>
2018-12-10 15:28:10 -0700

    Adding seed data

--- seeds/03Gear.js
+++ seeds/03Gear.js
@@ -1,13 +1,50 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('gear').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('gear').insert([
+        {name: '',
+        description: '',
+        price: ,
+        image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        },{name: '',
+          description: '',
+          price: ,
+          image: ''
+        }
       ]);
     });
 };
